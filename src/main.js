55e41b7dca118a5b3f9306b5e9e9103e19911341
Jazziel Puente <jazziel.puente.v@gmail.com>
2022-05-27 10:58:40 -0600

    Agregando la lista de categorías al DOM

--- src/main.js
+++ src/main.js
@@ -18,4 +18,23 @@ const getTrendingMoviesPreview = async () => {
     trendingPreviewMovieList.innerHTML = listMovies.join("");
 }
 
+const getCategoriesPreviw = async () => {
+    const categoriesPreviewList = document.querySelector("#categoriesPreviewList");
+    const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY);
+    const data = await res.json();
+
+    const genres = data.genres;
+
+    const HtmlGenres = genres.map(genre => {
+        return `<div class="category-container">
+                <h3 id="id${genre.id}" class="category-title">${genre.name}</h3>
+                </div>`;
+    });
+    
+
+    categoriesPreviewList.innerHTML = HtmlGenres.join("");
+
+}
+
 getTrendingMoviesPreview();
+getCategoriesPreviw();
